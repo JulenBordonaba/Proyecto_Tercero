f0c166b8797a3a5edcdfa8942a895aca40d79310
Julen Bordonaba <48485297+JulenBordonaba@users.noreply.github.com>
2019-10-07 18:12:41 +0200

    Clase combustible creada

---
+++ Proyecto Pruebas/Assets/_MisAssets/Scripts/Combustible.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Combustible : MonoBehaviour
+{
+    public Color color;
+    public float deposit;
+    public float pasiveConsumption;
+    public float activeConsumption;
+    public float duration;
+
+    public abstract void Use();
+
+}
