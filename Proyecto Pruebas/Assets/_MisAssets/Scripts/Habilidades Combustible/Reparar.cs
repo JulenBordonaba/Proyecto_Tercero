6f21ced54d60f77fddf8cd4a876793d647031b9f
roboioritz <48485367+roboioritz@users.noreply.github.com>
2019-10-08 19:08:54 +0200

    Reparar empezado sin terminar

--- Proyecto Pruebas/Assets/_MisAssets/Scripts/Habilidades Combustible/Reparar.cs
+++ Proyecto Pruebas/Assets/_MisAssets/Scripts/Habilidades Combustible/Reparar.cs
@@ -4,8 +4,56 @@ using UnityEngine;
 
 public class Reparar : HabilidadCombustible
 {
+    private bool repairing = false;
+
     public override void Use()
     {
-        print("Usa Reparar");
+        if (repairing == false) { 
+
+        Combustible combustibleReparar = new Combustible(); //variable para guardar el componente combustible de reparacion del objeto padre
+                      
+        //activar animacion reparacion
+        //GetComponentInParent<Animator>().SetBool("repair",true);
+
+        //activar sonido reparqacion
+        //GetComponentInParent<AudioSource>().Play();
+
+        //codigo que busca entre todos los combustibles del objeto y guarda el combustible del escudo. 
+        //Así se pueden acceder a las variables del combustible del escudo
+
+        Component[] combustibles;
+        combustibles = GetComponentsInParent(typeof(Combustible));
+        if (combustibles != null)
+        {
+            foreach (Combustible combustible in combustibles)
+                if (combustible.tipoCombustible == TipoCombustible.Reparar)
+                {
+                    combustibleReparar = combustible;
+                }
+        }
+        else
+        {
+            //lo que sea
+        }
+
+            repairing = true;
+        StartCoroutine(Repairs(combustibleReparar.duration));
+        }
+    }
+
+    private IEnumerator Repairs(float waitTime)
+    {
+        /*
+         * 
+         * 
+         * 
+         * 
+         */
+
+        yield return new WaitForSeconds(waitTime);
+
+        //desactivar variables de control de estado escudo
+        repairing = false;
+        //GetComponentInParent<Animator>().SetBool("inShield",false);
     }
 }
