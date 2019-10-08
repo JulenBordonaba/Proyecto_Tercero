b4a4e3fca3409c6579459dd705f7cc5579cc8a8b
Julen Bordonaba <48485297+JulenBordonaba@users.noreply.github.com>
2019-10-08 13:49:08 +0200

    Creado NaveManager

--- Proyecto Pruebas/Assets/_MisAssets/Scripts/NaveManager.cs
+++ Proyecto Pruebas/Assets/_MisAssets/Scripts/NaveManager.cs
@@ -4,8 +4,19 @@ using UnityEngine;
 
 public class NaveManager : MonoBehaviour
 {
-    
 
+    public string[] combustibles;
+    public HabilidadCombustible habilidadCombustible;
+    private int combustibleActivo = 0;
+    public bool inShield = false;
 
-    
+    private void AsignarCombustibleInicial()
+    {
+
+    }
+
+    private void CombustibleManager()
+    {
+
+    }
 }
