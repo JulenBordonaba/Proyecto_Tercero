1d4e7fb923c3614d7caf7f6d1a578be968b27d99
Iriadien <42410087+Iriadien@users.noreply.github.com>
2019-10-08 13:18:28 +0200

    Script Stats
    estadisticas genericas para todos los objetos

---
+++ Proyecto Pruebas/Assets/_MisAssets/Scripts/Stats.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stats : MonoBehaviour
+{
+    [Tooltip("Asigna la vida del objeto")]
+    public float life;   //vida que tendrá el objeto
+    [Tooltip("Asigna la cantidad de daño que va a hacer al chocar")]
+    public float collisionDamage;     //daño por colision
+    [Tooltip("Asigna la cantidad de daño que va a hacer al disparar. Si no dispara pon 0")]
+    public float shotDamage;     //daño por disparo
+    [Tooltip("Asigna el peso que tiene el objeto")]
+    public float weight;      //pero del objeto. Influye en su daño y velocidad
+}
