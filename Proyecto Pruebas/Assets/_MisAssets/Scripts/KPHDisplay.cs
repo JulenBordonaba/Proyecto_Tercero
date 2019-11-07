dc066ad4d33164ad1c3b185befd8c692b3b48c07
Iriadien <42410087+Iriadien@users.noreply.github.com>
2019-11-07 19:56:51 +0100

    kph

---
+++ Proyecto Pruebas/Assets/_MisAssets/Scripts/KPHDisplay.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class KPHDisplay : MonoBehaviour
+{
+    public Rigidbody rb;
+    public Text display_Text;
+
+    public void Update()
+    {
+        display_Text.text = (int)rb.velocity.magnitude + " KPH";
+    }
+}
