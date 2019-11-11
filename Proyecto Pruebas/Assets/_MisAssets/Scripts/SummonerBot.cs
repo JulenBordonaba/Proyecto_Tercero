547d7f9301b7d13f84991aadb6c4599ce8a6fa1b
Julen Bordonaba <48485297+JulenBordonaba@users.noreply.github.com>
2019-11-11 13:53:19 +0100

    Bot se redirige y va aumentando la velocidad

--- Proyecto Pruebas/Assets/_MisAssets/Scripts/SummonerBot.cs
+++ Proyecto Pruebas/Assets/_MisAssets/Scripts/SummonerBot.cs
@@ -8,27 +8,44 @@ public class SummonerBot : MonoBehaviour
     public float velocity;
     [Tooltip("Pon las layers de los objetos que el dron debe tomar como objetivo")]
     public LayerMask hitLayers;
+    [Tooltip("Pon la corrección de la trayectoria del bot")]
+    public float trayectoryCorrection = 1;
+    [Tooltip("Pon el aumento de velocidad del bot respecto al tiempo")]
+    public float velocityIncrease = 10;
 
     private Animator animator;  //animator del bot
     private Vector3 direction;
     private bool inShot = false;
+    private Transform objective;
     // Start is called before the first frame update
     void Start()
     {
         animator = transform.parent.GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
         if (!inShot) return;
+        velocity += velocityIncrease * Time.deltaTime;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (!inShot) return;
+        Redirect();
         Move();
 
     }
 
+    private void Redirect()
+    {
+        direction = Vector3.Lerp(direction, (objective.position - transform.position).normalized, Time.deltaTime * trayectoryCorrection).normalized;
+    }
+
     private void Move()
     {
-        transform.parent.Translate(direction * velocity * Time.deltaTime);
+        transform.parent.Translate(direction * velocity * Time.fixedDeltaTime);
     }
 
     private void OnTriggerStay(Collider other)
@@ -49,8 +66,10 @@ public class SummonerBot : MonoBehaviour
                 {
                     if (((1 << hit.transform.gameObject.layer) & hitLayers) != 0)
                     {
+                        direction = (other.transform.position - transform.position).normalized;
                         animator.enabled = false;
                         inShot = true;
+                        objective = other.transform;
                         transform.parent.parent.SetParent(null);
                         GetComponentInParent<SummonerBotCollision>().inShot = true;
                     }
