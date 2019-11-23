019bdd1e8d1d2ccf6d3413fbc5de19faddbebbc1
Julen Bordonaba <48485297+JulenBordonaba@users.noreply.github.com>
2019-11-24 00:44:05 +0100

    cambios indicador checkpoint

--- Proyecto Pruebas/Assets/_MisAssets/Scripts/UIManager.cs
+++ Proyecto Pruebas/Assets/_MisAssets/Scripts/UIManager.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class UIManager : MonoBehaviour
 {
     public Image checkpointImage;
-
+    public float checkpointDamping = 5;
     private Camera myCamera;
 
 
@@ -25,22 +25,24 @@ public class UIManager : MonoBehaviour
     private void ShowNewestCheckpoint()
     {
         print(CheckpointScreenPosition);
-        checkpointImage.GetComponent<RectTransform>().localPosition = new Vector3(Mathf.Clamp(CheckpointScreenPosition.x - (Screen.width * 0.5f), -Screen.width * 0.5f, Screen.width * 0.5f), Mathf.Clamp(CheckpointScreenPosition.y - (Screen.height * ((myCamera.rect.height * 0.5f) + myCamera.rect.y)), -(Screen.height * myCamera.rect.height * 0.5f), (Screen.height * myCamera.rect.height * 0.5f)), 0);
+        Vector3 auxPos;
+        auxPos = new Vector3(Mathf.Clamp(CheckpointScreenPosition.x - (Screen.width * 0.5f), -Screen.width * 0.5f, Screen.width * 0.5f), Mathf.Clamp(CheckpointScreenPosition.y - (Screen.height * ((myCamera.rect.height * 0.5f) + myCamera.rect.y)), -(Screen.height * myCamera.rect.height * 0.5f), (Screen.height * myCamera.rect.height * 0.5f)), 0);
         if (CheckpointScreenPosition.z < 0)
         {
-            Vector3 pos = checkpointImage.GetComponent<RectTransform>().localPosition;
+            Vector3 pos = auxPos;
 
 
 
             if (pos.x > -(Screen.width * 0.5f) || pos.x < (Screen.width * 0.5f))
             {
-                checkpointImage.GetComponent<RectTransform>().localPosition = new Vector3(pos.x, (Screen.height * myCamera.rect.height * 0.5f), 0);
+                auxPos = new Vector3(pos.x, (Screen.height * myCamera.rect.height * 0.5f), 0);
             }
             else
             {
-                checkpointImage.GetComponent<RectTransform>().localPosition = new Vector3(pos.x, -pos.y, 0);
+                auxPos = new Vector3(pos.x, -pos.y, 0);
             }
         }
+        checkpointImage.GetComponent<RectTransform>().localPosition = Vector3.Lerp(checkpointImage.GetComponent<RectTransform>().localPosition, auxPos, Time.deltaTime*checkpointDamping);
     }
 
     private Vector3 CheckpointScreenPosition
