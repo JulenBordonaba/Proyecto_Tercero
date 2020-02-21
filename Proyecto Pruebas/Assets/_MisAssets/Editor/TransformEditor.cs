cccde038e1f5fb89df8c53f1f4fd91d8aa5a62e6
Julen Bordonaba <48485297+JulenBordonaba@users.noreply.github.com>
2020-02-21 17:16:47 +0100

    mejora herramienta añadir componentes

---
+++ Proyecto Pruebas/Assets/_MisAssets/Editor/TransformEditor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Transform))]
+public class TransformEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        Transform t = (Transform)target;
+
+        if (GUILayout.Button("Add Copy Components"))
+        {
+            t.gameObject.AddComponent<CopyComponents>();
+        }
+    }
+}
