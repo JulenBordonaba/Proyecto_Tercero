66a16a64f6ac111a235a46cbbdacaad414e06820
Julen Bordonaba <48485297+JulenBordonaba@users.noreply.github.com>
2020-04-20 15:26:32 +0200

    script invertir normales y poligonos

---
+++ Proyecto Pruebas/Assets/_MisAssets/Editor/ReverseNormalsEditor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(ReverseNormals))]
+public class ReverseNormalsEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        ReverseNormals rn = (ReverseNormals)target;
+
+        GUILayout.Space(10);
+        if (GUILayout.Button("Reverse Normals"))
+        {
+            rn.ReverseNormal();
+        }
+
+        GUILayout.Space(10);
+        if (GUILayout.Button("Reverse Polygons"))
+        {
+            rn.ReversePolygons();
+        }
+
+
+
+    }
+}
---
+++ Proyecto Pruebas/Assets/_MisAssets/Scripts/ReverseNormals.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.Linq;
+
+
+[RequireComponent(typeof(MeshFilter))]
+public class ReverseNormals : MonoBehaviour
+{
+    [ContextMenu("Reverse normals")]
+    public void ReverseNormal()
+    {
+        MeshFilter filter = GetComponent(typeof(MeshFilter)) as MeshFilter;
+        if (filter != null)
+        {
+            Mesh mesh = filter.sharedMesh;
+
+            Vector3[] normals = mesh.normals;
+            for (int i = 0; i < normals.Length; i++)
+                normals[i] = -normals[i];
+            mesh.normals = normals;
+
+            for (int m = 0; m < mesh.subMeshCount; m++)
+            {
+                int[] triangles = mesh.GetTriangles(m);
+                for (int i = 0; i < triangles.Length; i += 3)
+                {
+                    int temp = triangles[i + 0];
+                    triangles[i + 0] = triangles[i + 1];
+                    triangles[i + 1] = temp;
+                }
+                mesh.SetTriangles(triangles, m);
+            }
+        }
+    }
+
+
+    [ContextMenu("Reverse poligons")]
+    public void ReversePolygons()
+    {
+
+        Mesh mesh = GetComponent<MeshFilter>().sharedMesh;
+        mesh.triangles = mesh.triangles.Reverse().ToArray();
+    }
+}
