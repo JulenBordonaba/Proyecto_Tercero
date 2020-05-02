cab63eaa053987006c924db608272be0cf35c568
Iriadien <42410087+Iriadien@users.noreply.github.com>
2020-05-02 12:56:43 +0200

    menu garaje

---
+++ Proyecto Pruebas/Assets/_MisAssets/Scripts/Menus/UISelect.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class UISelect : EventTrigger
+{
+
+    public override void OnSelect(BaseEventData eventData)
+    {
+        base.OnSelect(eventData);
+        // Call methods when the UI element is selected
+        //GetComponent<Animator>().SetTrigger("Selected");
+        GetComponent<Animator>().SetBool("Selected",true);
+    }
+
+    public override void OnDeselect(BaseEventData eventData)
+    {
+        base.OnDeselect(eventData);
+        // Call methods when the UI element is deselected
+        //GetComponent<Animator>().ResetTrigger("Selected");
+        GetComponent<Animator>().SetBool("Selected",false);
+    }
+}
