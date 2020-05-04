c88d54b66bacacbc65f0a00cd749d0d689dd3053
Iriadien <42410087+Iriadien@users.noreply.github.com>
2020-05-04 16:33:32 +0200

    raton casi funcional en menus principal y garaje

---
+++ Proyecto Pruebas/Assets/_MisAssets/Scripts/Menus/UIFocus.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class UIFocus : MonoBehaviour, IPointerDownHandler
+{
+    public Button defaultButton;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("vuelve a coger el foco");
+        defaultButton.Select();
+    }
+}
