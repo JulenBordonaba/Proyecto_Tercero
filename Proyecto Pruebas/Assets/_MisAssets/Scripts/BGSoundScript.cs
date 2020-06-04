ef287a520fecb7ebd1a93392073cc6a8f1fd48ae
Carlos Miguelez <carlosmiguelez404@gmail.com>
2020-06-04 03:18:14 +0200

    Sonidos
    Menus casi listos
    Falta el sonido de escena para alante escena para atras
    Falta el sonido de levitacion de las naves en el garaje
    
    Faltan todos los sonidos en el juego

---
+++ Proyecto Pruebas/Assets/_MisAssets/Scripts/BGSoundScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class BGSoundScript : MonoBehaviour
+{
+    //AudioSource myAudio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*myAudio = GetComponent<AudioSource>();
+        if (myAudio.mute == true)
+        {
+            myAudio.mute = false;
+        }*/
+    }
+
+    //Play Global
+    private static BGSoundScript instance = null;
+    public static BGSoundScript Instance
+    {
+        get { return instance;  }
+    }
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+    }
+}
