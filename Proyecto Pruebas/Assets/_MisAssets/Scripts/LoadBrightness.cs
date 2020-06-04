feb6ae916e933ef6caae01d0690b458ff8ac1b10
Julen Bordonaba <48485297+JulenBordonaba@users.noreply.github.com>
2020-06-05 00:34:13 +0200

    brillo y clones hacker

--- Proyecto Pruebas/Assets/_MisAssets/Scripts/LoadBrightness.cs
+++ Proyecto Pruebas/Assets/_MisAssets/Scripts/LoadBrightness.cs
@@ -7,7 +7,7 @@ public class LoadBrightness : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        RenderSettings.ambientLight = new Color(OptionsMenu.settings.brightness, OptionsMenu.settings.brightness, OptionsMenu.settings.brightness, 1f);
     }
 
     // Update is called once per frame
